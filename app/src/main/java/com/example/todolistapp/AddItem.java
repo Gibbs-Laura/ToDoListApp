4e072a4a9a0702f53eb33df77ba482ecb8839a09
Laura G <valeriogibbs89@gmail.com>
2020-09-14 17:19:00 -0400

    creating the main activity layout

--- app/src/main/java/com/example/todolistapp/AddItem.java
+++ app/src/main/java/com/example/todolistapp/AddItem.java
@@ -56,7 +56,7 @@ public class AddItem extends AppCompatActivity  {
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_add_task);
+        setContentView(R.layout.add_item);
 
         initViews();
 
